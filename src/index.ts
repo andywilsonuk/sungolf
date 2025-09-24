67f6261ae0b366d98ba029f1c2105a1cdacb6470
andywilsonuk <code@andywilson.me.uk>
2025-09-24 19:35:45 +0100

    Refactor loading screen implementation

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,19 @@ const showGameUI = (): void => {
   const loadingScreen = document.getElementById('loadingScreen')
   const gameArea = document.getElementById('gameArea')
   const optionsButton = document.getElementById('optionsButton')
+  const options = document.getElementById('options')
 
   if (loadingScreen) {
     loadingScreen.style.display = 'none'
   }
-
   if (gameArea) {
-    gameArea.classList.remove('hide')
+    gameArea.style.display = ''
   }
-
   if (optionsButton) {
-    optionsButton.classList.remove('hide')
+    optionsButton.style.display = ''
+  }
+  if (options) {
+    options.style.display = ''
   }
 }
 
@@ -28,8 +30,6 @@ const load = async (): Promise<void> => {
   await loadSounds(soundList)
   createWorld()
   start()
-
-  // Show the game UI once everything is loaded
   showGameUI()
 }
 
